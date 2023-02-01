d52f535a8c701ac7523d7f87629c25888a6969f4
Marta Aramu <marta.aramu99@gmail.com>
2023-02-01 17:20:53 +0100

    modifiche FactorialController

--- src/main/java/co/develhope/firstapi2/controllers/FactorialController.java
+++ src/main/java/co/develhope/firstapi2/controllers/FactorialController.java
@@ -2,12 +2,19 @@ package co.develhope.firstapi2.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RestController //rende il controller un controller rest, dà risposta alle richieste e utilizza il protocollo https
+@RequestMapping("/factorial")
 public class FactorialController {
 
-    @GetMapping("/{n}")
+    @GetMapping
+    public String getDefaultMessage(){
+        return "ciao";
+    }
+
+    @GetMapping("/calcfattoriale/{n}")
     public Integer getFactorial(@PathVariable (name = "n") Integer n){
         Integer result = n;
         while (n>1){
